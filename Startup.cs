740c0c4e19a7583be451e2184eee4711c9a789e1
normabunton <te_nj25@hotmail.com>
2021-03-21 13:11:31 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRaterRedBadge.Startup))]
+namespace RestaurantRaterRedBadge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
